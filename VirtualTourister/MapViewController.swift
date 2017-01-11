4eeffb5456e258751c0ced3c19ddf95739b1c9b3
Midhet Sulemani <midhetsulemani@Midhets-MacBook-Air.local>
2017-01-11 15:23:06 +0530

    ns user defaults done

--- VirtualTourister/MapViewController.swift
+++ VirtualTourister/MapViewController.swift
@@ -9,6 +9,7 @@
 import UIKit
 import MapKit
 import CoreData
+import Foundation
 
 class MapViewController: UIViewController, MKMapViewDelegate {
     
@@ -22,6 +23,18 @@ class MapViewController: UIViewController, MKMapViewDelegate {
         let longPress = UILongPressGestureRecognizer(target: self, action: #selector(addPin(sender:)))
         map.addGestureRecognizer(longPress)
         map.delegate = self
+        
+        if UserDefaults.standard.bool(forKey: "mapRegion") {
+            
+            let latDelta = UserDefaults.standard.value(forKey: "latDelta")
+            let longDelta = UserDefaults.standard.value(forKey: "longDelta")
+            let lat = UserDefaults.standard.value(forKey: "lat")
+            let long = UserDefaults.standard.value(forKey: "long")
+            let coordinate = CLLocationCoordinate2D(latitude: lat as! CLLocationDegrees, longitude: long as! CLLocationDegrees)
+            let span = MKCoordinateSpanMake(latDelta as! CLLocationDegrees, longDelta as! CLLocationDegrees)
+            let region = MKCoordinateRegionMake(coordinate, span)
+            map.setRegion(region, animated: false)
+        }
     }
 
     func addPin(sender: UILongPressGestureRecognizer) {
@@ -32,6 +45,16 @@ class MapViewController: UIViewController, MKMapViewDelegate {
         annotation.coordinate = location
         annotations.append(annotation)
         map.addAnnotations(annotations)
+        
+        let span = map.region.span
+        let region = map.region
+        print("delta is:", span.latitudeDelta, span.longitudeDelta)
+        print("region is:", region)
+        UserDefaults.standard.set(map.region.span.latitudeDelta, forKey: "latDelta")
+        UserDefaults.standard.set(map.region.span.longitudeDelta, forKey: "longDelta")
+        UserDefaults.standard.set(map.region.center.latitude, forKey: "lat")
+        UserDefaults.standard.set(map.region.center.longitude, forKey: "long")
+        UserDefaults.standard.set(true, forKey: "mapRegion")
     }
     
     func savePin(node: MKAnnotation) {
@@ -48,6 +71,84 @@ class MapViewController: UIViewController, MKMapViewDelegate {
         catch
         {
             print("error in saving context")
+        }
+    }
+    
+    var mapChangedFromUserInteraction = false
+    
+    func mapViewRegionDidChangeFromUserInteraction() -> Bool {
+        let view = map.subviews[0]
+        //  Look through gesture recognizers to determine whether this region change is from user interaction
+        if let gestureRecognizers = view.gestureRecognizers {
+            for recognizer in gestureRecognizers {
+                if( recognizer.state == .began || recognizer.state == .ended ) {
+                    return true
+                }
+            }
+        }
+        return false
+    }
+    
+    func mapView(_ mapView: MKMapView, regionWillChangeAnimated animated: Bool) {
+        mapChangedFromUserInteraction = mapViewRegionDidChangeFromUserInteraction()
+        if (mapChangedFromUserInteraction) {
+            // user will change map region
+            print("user WILL change map.")
+            
+            // calculate the width of the map in miles.
+            let mRect: MKMapRect = mapView.visibleMapRect
+            let eastMapPoint = MKMapPointMake(MKMapRectGetMinX(mRect), MKMapRectGetMidY(mRect))
+            let westMapPoint = MKMapPointMake(MKMapRectGetMaxX(mRect), MKMapRectGetMidY(mRect))
+            let currentDistWideInMeters = MKMetersBetweenMapPoints(eastMapPoint, westMapPoint)
+            let milesWide = currentDistWideInMeters / 1609.34  // number of meters in a mile
+            print(milesWide)
+            print("^miles wide")
+            
+            // check if user zoomed in too far and zoom them out.
+            if milesWide < 2.0 {
+                var region:MKCoordinateRegion = mapView.region
+                var span:MKCoordinateSpan = mapView.region.span
+                span.latitudeDelta = 0.04
+                span.longitudeDelta = 0.04
+                region.span = span;
+                mapView.setRegion(region, animated: true)
+                print("map zoomed back out")
+            }
+            
+        }
+    }
+    
+    func mapView(_ mapView: MKMapView, regionDidChangeAnimated animated: Bool) {
+        
+        if (mapChangedFromUserInteraction) {
+            // user changed map region
+            print("user CHANGED map.")
+            print(mapView.region.span.latitudeDelta)
+            print(mapView.region.span.longitudeDelta)
+            
+            // calculate the width of the map in miles.
+            let mRect: MKMapRect = mapView.visibleMapRect
+            let eastMapPoint = MKMapPointMake(MKMapRectGetMinX(mRect), MKMapRectGetMidY(mRect))
+            let westMapPoint = MKMapPointMake(MKMapRectGetMaxX(mRect), MKMapRectGetMidY(mRect))
+            let currentDistWideInMeters = MKMetersBetweenMapPoints(eastMapPoint, westMapPoint)
+            let milesWide = currentDistWideInMeters / 1609.34  // number of meters in a mile
+            print(milesWide)
+            print("^miles wide")
+            
+            // check if user zoomed in too far and zoom them out.
+            if milesWide < 2.0 {
+                
+                DispatchQueue.main.async {
+                    
+                    var region:MKCoordinateRegion = mapView.region
+                    var span:MKCoordinateSpan = mapView.region.span
+                    span.latitudeDelta = 0.04
+                    span.longitudeDelta = 0.04
+                    region.span = span;
+                    mapView.setRegion(region, animated: true)
+                    print("map zoomed back out")
+                }
+            }
         }
     }
     
